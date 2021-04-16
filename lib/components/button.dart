75cd7db36a4646d77876e63dbfa1ee8d578ba2e6
Raka Yuda <raka.yuda.pradipta@gmail.com>
2021-04-17 05:56:55 +0700

    implement custom button on welcome page

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -8,35 +8,36 @@ class ButtonPrimary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return ElevatedButton(
         style: ElevatedButton.styleFrom(
             shape: const RoundedRectangleBorder(
               borderRadius: BorderRadius.all(Radius.circular(40)),
             ),
             primary: Color(0xFF31CD9D),
-            padding: EdgeInsets.symmetric(horizontal: 46, vertical: 32),
+            padding: EdgeInsets.symmetric(vertical: 16),
             textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
         onPressed: onPressed,
-        child: Expanded(
-          child: Container(
-            decoration: BoxDecoration(
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                  color: Color(0xFF31CD9D).withOpacity(0.2),
-                  blurRadius: 30,
-                  offset: Offset(0, 16),
-                ),
-              ],
-            ),
-            child: Text(
-              text,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  fontFamily: 'Nunito',
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w300),
-            ),
+        child: Container(
+          width: size.width,
+          decoration: BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Color(0xFF31CD9D).withOpacity(0.2),
+                blurRadius: 30,
+                offset: Offset(0, 16),
+              ),
+            ],
+          ),
+          child: Text(
+            text,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontFamily: 'Nunito',
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w300),
           ),
         ));
   }
--- lib/screens/welcome-page.dart
+++ lib/screens/welcome-page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:groups/components/button.dart';
 import 'package:groups/screens/login-page.dart';
 import 'package:groups/screens/register-page.dart';
 
@@ -9,135 +10,97 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
 
-    return Container(
-      width: size.width,
-      height: size.height,
-      color: Colors.white,
-      child: Center(
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Color(0xFFFAFAFA),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
+            Flexible(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
                 children: [
                   Container(
                     height: 96,
                     width: 96,
-                    child: Center(
-                      child: Text('G',
-                          style: TextStyle(
-                              fontFamily: 'Nunito',
-                              fontSize: 48,
-                              fontWeight: FontWeight.w900,
-                              color: Colors.black38)),
-                    ),
                     decoration: BoxDecoration(
-                      color: Colors.white,
+                      gradient: LinearGradient(
+                          begin: Alignment.topCenter,
+                          end: Alignment.bottomCenter,
+                          colors: [Color(0xFF00979B), Color(0xFF31CD9D)]),
                       borderRadius: BorderRadius.circular(12),
                       boxShadow: [
                         BoxShadow(
                           color: Colors.grey.withOpacity(0.2),
                           spreadRadius: 5,
                           blurRadius: 16,
-                          offset: Offset(5, 5), // changes position of shadow
+                          offset: Offset(5, 5),
                         ),
                       ],
+                    ),
+                    child: Center(
+                      child: Text('G',
+                          style: TextStyle(
+                              fontFamily: 'Nunito',
+                              fontSize: 48,
+                              fontWeight: FontWeight.w900,
+                              color: Colors.white)),
                     ),
                   ),
                   SizedBox(
                     height: 64,
                   ),
                   Text(
+                    'Welcome to Groups',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontFamily: 'Nunito',
+                        color: Color(0xFF2F4858),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
                     'A flutter live chat application to make a \n new relationship with a stranger',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                         fontFamily: 'Nunito',
-                        color: Colors.black,
-                        fontSize: 18,
+                        color: Color(0xFF2F4858),
+                        fontSize: 16,
                         fontWeight: FontWeight.w300),
-                  ),
-                  SizedBox(
-                    height: 64,
                   ),
                 ],
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                      side: BorderSide(width: 1, color: Colors.grey),
-                      primary: Colors.white,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 46, vertical: 32),
-                      textStyle:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                  onPressed: () {
-                    Navigator.pushNamed(context, LoginPage.id);
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.app_registration,
-                        color: Colors.blueAccent,
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Login with Email',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontFamily: 'Nunito',
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w300),
-                        ),
-                      )
-                    ],
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-              child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      shape: const RoundedRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(40)),
-                      ),
-                      side: BorderSide(width: 1, color: Colors.grey),
-                      primary: Colors.white,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 46, vertical: 32),
-                      textStyle:
-                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegisterPage.id);
-                  },
-                  child: Row(
-                    children: [
-                      Icon(
-                        Icons.app_registration,
-                        color: Colors.blueAccent,
-                      ),
-                      Expanded(
-                        child: Text(
-                          'Register with Email',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                              fontFamily: 'Nunito',
-                              color: Colors.black,
-                              fontSize: 20,
-                              fontWeight: FontWeight.w300),
-                        ),
-                      )
-                    ],
-                  )),
-            ),
-            SizedBox(
-              height: 64,
-            ),
+            Flexible(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  child: ButtonPrimary(
+                    text: 'Login with Email',
+                    onPressed: () {
+                      Navigator.pushNamed(context, LoginPage.id);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                  child: ButtonPrimary(
+                    text: 'Register with Email',
+                    onPressed: () {
+                      Navigator.pushNamed(context, RegisterPage.id);
+                    },
+                  ),
+                ),
+              ],
+            )),
           ],
         ),
       ),
